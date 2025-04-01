22bd183a0779d1a05329a665816c2bb52ebe58e1
nacchan-kun <sendono.nc17@gmail.com>
2025-04-01 13:51:16 +0300

    fix

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,7 +1,6 @@
-// Описаний в документації
+
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+
 
 const images = [
   {
